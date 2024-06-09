74b2c32b42df8e25355a9bd9920b2eb45d26c460
Tahacollege <tcollegewala30@gmail.com>
2024-06-09 21:20:02 +0530

    final changes

--- src/Projects.js
+++ src/Projects.js
@@ -16,40 +16,42 @@ export default function Projects(){
             <div className="majorp" data-aos="zoom-in" data-aos-easing="ease-out-cubic"
      data-aos-duration="4000">
                 <div className="forums" >
+                    
+                    <a href="https://iforumspr.000webhostapp.com/"><img alt="" src="./projects/forums_1.png" className="forumsimg"></img></a>
                     <div className="content">
                         <h1>Name:Coding Forums</h1>
-                        <h2>LanguagesUsed:HTML,CSS,JAVASCRIPT,PHP</h2>
+                        <h2>Languages Used:HTML,CSS,JAVASCRIPT,PHP</h2>
                         <h3>Type:Dynamic Website</h3>
                         <h4>Database:Mysql</h4>
                     </div>
-                    <a href="https://iforumspr.000webhostapp.com/"><img alt="" src="./projects/forums_1.png" className="forumsimg"></img></a>
                 </div>
                 <div className="elibrary" >
+                <a href="https://unparallel-umbrella.000webhostapp.com/"><img alt="" src="./projects/elibrary.png" className="elibraryimg"></img></a>
                 <div className="content">
                         <h1>Name:Elibrary</h1>
-                        <h2>LanguagesUsed:HTML,CSS,JAVASCRIPT,PHP</h2>
+                        <h2>Languages Used:HTML,CSS,JAVASCRIPT,PHP</h2>
                         <h3>Type:Dynamic Website</h3>
                         <h4>Database:Mysql</h4>
                     </div>
-                <a href="https://unparallel-umbrella.000webhostapp.com/"><img alt="" src="./projects/elibrary.png" className="elibraryimg"></img></a>
                 </div>
+                
                 <div className="blogers" >
+                    <a href="https://blogerscomers.000webhostapp.com//"><img alt="" src="./projects/bloggers.png" className="blogersimg"></img></a>
                     <div className="content">
                         <h1>Name:Blogers</h1>
-                        <h2>LanguagesUsed:HTML,CSS,JAVASCRIPT,PHP</h2>
+                        <h2>Languages Used:HTML,CSS,JAVASCRIPT,PHP</h2>
                         <h3>Type:Dynamic Website</h3>
                         <h4>Database:Mysql</h4>
                     </div>
-                    <a href="https://blogerscomers.000webhostapp.com//"><img alt="" src="./projects/bloggers.png" className="blogersimg"></img></a>
                 </div>
                 <div className="project_manager" >
+                    <a href="https://prmanagehub.000webhostapp.com/index.php/"><img alt="" src="./projects/project_management.png" className="project_managerimg"></img></a>
                     <div className="content">
                         <h1>Name:Project Manager</h1>
-                        <h2>LanguagesUsed:HTML,CSS,JAVASCRIPT,PHP</h2>
+                        <h2>Languages Used:HTML,CSS,JAVASCRIPT,PHP</h2>
                         <h3>Type:Dynamic Website</h3>
                         <h4>Database:Mysql</h4>
                     </div>
-                    <a href="https://prmanagehub.000webhostapp.com/index.php/"><img alt="" src="./projects/project_management.png" className="project_managerimg"></img></a>
                 </div>
             </div>
             <hr />
@@ -60,31 +62,31 @@ export default function Projects(){
             <div className="minorp" data-aos="zoom-in" data-aos-easing="ease-out-cubic"
      data-aos-duration="4000">
                 <div className="automobiles" >
-                <div className="content">
-                        <h1>Name:Automobiles</h1>
-                        <h2>LanguagesUsed:HTML,CSS,Reactjs</h2>
-                        <h3>Type:Static Website</h3>
-                        <h4>Database:Mysql</h4>
-                    </div>
                 <a href="https://automobles-info.netlify.app/"><img alt="" src="./projects/automobiles.png" className="automobilesimg"></img></a>
+                <div className="content_minor">
+                        <h1>Name:Automobiles</h1>
+                        <h2 >Languages Used:HTML,CSS,Reactjs</h2>
+                        <h3>Type:Static Website</h3>
+                        <h4>Database:None</h4>
+                    </div>
                 </div>
                 <div className="overview" >
-                <div className="content">
-                        <h1>Name:Overview</h1>
-                        <h2 className="h2">LanguagesUsed:HTML,CSS,Reactjs</h2>
-                        <h3>Type:Static Website</h3>
-                        <h4>Database:Mysql</h4>
-                    </div>
                 <a href="https://theoverview.netlify.app/"><img alt="" src="./projects/overview.png" className="overviewimg"></img></a>
+                <div className="content_minor">
+                        <h1>Name:Overview</h1>
+                        <h2 >Languages Used:HTML,CSS,Reactjs</h2>
+                        <h3>Type:Static Website</h3>
+                        <h4>Database:None</h4>
+                    </div>
                 </div>
                 <div className="tenzies" >
-                <div className="content">
-                        <h1>Name:Tenzies</h1>
-                        <h2 className="h2">LanguagesUsed:HTML,CSS,Reactjs</h2>
-                        <h3>Type:Static Website</h3>
-                        <h4>Database:Mysql</h4>
-                    </div>
                 <a href="https://tenzies-g.netlify.app/"><img alt="" src="./projects/tenzies.png" className="tenziesimg"></img></a>
+                <div className="content_minor">
+                        <h1>Name:Tenzies</h1>
+                        <h2 >Languages Used:HTML,CSS,Reactjs</h2>
+                        <h3>Type:Static Website</h3>
+                        <h4>Database:None</h4>
+                    </div>
                 </div>
             </div>
             <hr />
